b48073f01b1b3a4bcc1bd58436f6afc03f7cbd7a
Ahmad Nazir Raja <mandark@mandark>
2022-10-10 21:02:18 +0200

    README and installation scripts

--- README.md
+++ README.md
@@ -1,41 +1,36 @@
 ## Build
 
+### Prerequisites
+
+```
+sudo apt install python3-pip
+pip3 install -U ablog
+```
+
+Hack: If the above doesn't work, install it from within the docker container
+with the home directory mounted.
+
+
+### Install
+
+```
+source ./scripts/scribbles.sh
+```
+
 ### Compile
 
 ```
-docker run -it --rm \
-  -v /etc/passwd:/etc/passwd \
-  -v /etc/group:/etc/group \
-  -v $HOME:$HOME \
-  -w `pwd` -v `pwd`:`pwd` \
-  -u $UID:$GID \
-  sphinxdoc/sphinx:5.0.1 make html man
+scribbles-compile
 ```
 
 ### Cleanup
 
 ```
-cd build
-mv html html_
-mkdir html
-touch html/.nojekyll
-rm html_ -r
-scribbles-compile
-cd -
+scribbles-clean
 ```
-
-where `scribbles-compile` is the bash function for the compile step.
 
 ## Publish
 
-### ahmadnazir.github.io/scribbles
-
 ```
-git push origin `git subtree split --prefix build/html source`:master --force
-```
-
-### man pages
-
-```
-sudo cp build/man/scribbles.7 /usr/share/man/man7/
+scribbles-publish
 ```
---
+++ scripts/scribbles.sh
@@ -0,0 +1,65 @@
+IMAGE=sphinxdoc/sphinx:5.0.1
+DIR=~/code/ahmadnazir/scribbles
+
+function print-info () {
+    text-info "Image: ${IMAGE}"
+}
+
+function scribbles-clean () {
+    cd $DIR/build > /dev/null
+    mv html html_
+    mkdir html
+    touch html/.nojekyll
+    rm html_ -r
+    cd - > /dev/null
+}
+
+function scribbles-compile () {
+    print-info
+
+    docker run -it --rm \
+           -v /etc/passwd:/etc/passwd \
+           -v /etc/group:/etc/group \
+           -v $HOME:$HOME \
+           -w $DIR -v $DIR:$DIR \
+           -u $UID:$GID \
+           $IMAGE make html man
+}
+
+function -scribbles-publish () {
+    cd $DIR > /dev/null
+
+    text-info 'SCRIBBLES :: Updating git repo https://github.com/ahmadnazir/scribbles'
+
+    git push
+
+    text-info 'SCRIBBLES :: Publishing site https://ahmadnazir.github.io/scribbles'
+
+    git push origin `git subtree split --prefix build/html source`:master --force
+
+    text-info 'SCRIBBLES :: Publishing man pages locally'
+
+    sudo cp build/man/scribbles.7 /usr/share/man/man7/
+    cd - > /dev/null
+}
+
+function scribbles-publish () {
+    echo Pre-authorization to install man pages
+    sudo echo Authorized || return;
+
+    scribbles-compile
+
+    # Detect un-committed changes
+    cd $DIR > /dev/null
+    git status | grep 'working tree clean'
+    RESULT=$?
+    if [ $RESULT -ne 0 ]; then
+
+        text-error 'SCRIBBLES :: Content cannot be published until changes are committed.'
+
+        git diff
+    else
+        -scribbles-publish
+    fi
+    cd - > /dev/null
+}
